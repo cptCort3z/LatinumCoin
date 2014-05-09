4cd337352e1910ab7058fcfa6b153be4f16c8caa
einewton <einewton@gmail.com>
2014-05-09 10:03:58 -0600

    Checked in Chain Params.

--- src/chainparams.cpp
+++ src/chainparams.cpp
@@ -42,11 +42,11 @@ public:
         // Build the genesis block. Note that the output of the genesis coinbase cannot
         // be spent as it did not originally exist in the database.
 		
-		//CBlock(hash=23b7c7959cfde48e7bebbec1761df82eb075aad3f85df667b5e29acffc8d2b76, ver=1, hashPrevBlock=0000000000000000000000000000000000000000000000000000000000000000, hashMerkleRoot=e1853c83b009fdee7733c0266e5731d59a6729c2d8f45437a7d53b49ab668046, nTime=1399585216, nBits=1d00ffff, nNonce=0, vtx=1)
-		//  CTransaction(hash=e1853c83b009fdee7733c0266e5731d59a6729c2d8f45437a7d53b49ab668046, ver=1, vin.size=1, vout.size=1, nLockTime=0)
-		//	CTxIn(COutPoint(0000000000000000000000000000000000000000000000000000000000000000, 4294967295), coinbase 04ffff001d01044c8c546865204e657720596f726b2054696d6573202d204d415920382c2032303134202d20426172636c61797320746f20536872696e6b20496e766573746d656e742042616e6b20616e642043726561746520616e20496e7465726e616c20e280984261642042616e6be28099207c2045696e6577746f6e2c20536861646f776d616e2c20785068616e746f6d78)
+		//CBlock(hash=00000000fc11987c881fd2935a54104b6a203042d88b4ed333c2ac742b588419, ver=1, hashPrevBlock=0000000000000000000000000000000000000000000000000000000000000000, hashMerkleRoot=fbc238e23568f60fa8ef1d48baa8a0a13d4ed9f081cd7997b760f125b76bc7c2, nTime=1399585216, nBits=1d00ffff, nNonce=3816623773, vtx=1)
+		//  CTransaction(hash=fbc238e23568f60fa8ef1d48baa8a0a13d4ed9f081cd7997b760f125b76bc7c2, ver=1, vin.size=1, vout.size=1, nLockTime=0)
+		//	CTxIn(COutPoint(0000000000000000000000000000000000000000000000000000000000000000, 4294967295), coinbase 04ffff001d01044c88546865204e657720596f726b2054696d6573202d204d415920382c2032303134202d20426172636c61797320746f20536872696e6b20496e766573746d656e742042616e6b20616e642043726561746520616e20496e7465726e616c20274261642042616e6b27207c2045696e6577746f6e2c20536861646f776d616e2c20785068616e746f6d78)
 		//	CTxOut(nValue=50.00000000, scriptPubKey=04678afdb0fe5548271967f1a67130)
-		//  vMerkleTree: e1853c83b009fdee7733c0266e5731d59a6729c2d8f45437a7d53b49ab668046 
+		// vMerkleTree: fbc238e23568f60fa8ef1d48baa8a0a13d4ed9f081cd7997b760f125b76bc7c2  
 
         const char* pszTimestamp = "The New York Times - MAY 8, 2014 - Barclays to Shrink Investment Bank and Create an Internal 'Bad Bank' | Einewton, Shadowman, xPhantomx";
         CTransaction txNew;
@@ -61,15 +61,23 @@ public:
         genesis.nVersion = 1;
         genesis.nTime    = 1399585216;
         genesis.nBits    = 0x1d00ffff;
-        genesis.nNonce   = 2275476893;
+        genesis.nNonce   = 3816623773;
 		
 		hashGenesisBlock = genesis.GetHash();
+		
+		/*
+		while (hashGenesisBlock > bnProofOfWorkLimit.getuint256()){
+			if (++genesis.nNonce==0) break;
+			hashGenesisBlock = genesis.GetHash();
+		}
+		*/
 		
 		printf("%s\n", hashGenesisBlock.ToString().c_str());
         printf("%s\n", genesis.hashMerkleRoot.ToString().c_str());
         printf("%x\n", bnProofOfWorkLimit.GetCompact());
+		printf("%u\n", genesis.nNonce);
 		
-        assert(hashGenesisBlock == uint256("fbe8d61daf10f84a82369fb3bb645f85637fc959aebe61a0ff385e8f138dfb5a"));
+        assert(hashGenesisBlock == uint256("00000000fc11987c881fd2935a54104b6a203042d88b4ed333c2ac742b588419"));
         assert(genesis.hashMerkleRoot == uint256("fbc238e23568f60fa8ef1d48baa8a0a13d4ed9f081cd7997b760f125b76bc7c2"));
 
         vSeeds.push_back(CDNSSeedData("latinumcoin.org", "seed.latinumcoin.org"));
